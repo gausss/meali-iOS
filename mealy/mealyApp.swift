22a2fc82fc513a58e241395724fc2cf082a2672c
gausss <gausssven@gmail.com>
2022-10-01 17:48:06 +0200

    init app project

---
+++ mealy/mealyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealyApp.swift
+//  mealy
+//
+//  Created by Sven Gauß on 01.10.22.
+//
+
+import SwiftUI
+
+@main
+struct mealyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
